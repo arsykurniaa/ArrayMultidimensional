25a0202ae8ae11b4bd24dbd68c4cb181e6ed8a91
arsykurniaa <arsykurniaa@student.ub.ac.id>
2022-12-12 23:04:40 +0700

    first commit

---
+++ src/main/java/com/mycompany/tempatduduk/TempatDuduk.java
@@ -0,0 +1,30 @@
+package com.mycompany.tempatduduk;
+import java.util.Scanner;
+
+public class TempatDuduk {
+
+    public static void main(String[] args) {
+       
+        // Membuat Array dan Scanner [baris dan kolom ]
+        String[][] kursi = new String[5][2];
+        Scanner input = new Scanner(System.in);
+        
+        // mengisi setiap kursi
+        for(int banjar = 0; banjar < kursi.length; banjar++){
+            for(int saff = 0; saff < kursi[banjar].length; saff ++){
+                System.out.print("Siapa yang akan duduk di kursi ("+banjar+","+saff+") : ");
+                kursi[banjar][saff] = input.nextLine();
+            }
+        }
+        
+         // menampilkan isi Array
+        System.out.println("-------------------------");
+        for(int banjar = 0; banjar < kursi.length; banjar++){
+            for(int saff = 0; saff < kursi[banjar].length; saff++){
+                System.out.print("| "+ kursi[banjar][saff]+" |");
+            }
+            System.out.println("");
+        }
+        System.out.println("-------------------------");
+    }
+}
